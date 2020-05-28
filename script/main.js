56c87e3b0f4d631e59b72144905b351f71a03e04
Denis <niskadevla@gmail.com>
2020-05-28 19:00:53 +0300

    Day 3

--- script/main.js
+++ script/main.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
-const API_KEY = '2d4c165832ba9416909b2d8884eb29e3';
 
 const leftMenu = document.querySelector('.left-menu'),
   hamburger = document.querySelector('.hamburger'),
   tvShowsList = document.querySelector('.tv-shows__list'),
-  modal = document.querySelector('.modal');
+  modal = document.querySelector('.modal'),
+  tvShows = document.querySelector('.tv-shows'),
+  tvCardImg = document.querySelector('.tv-card__img'),
+  modalTitle = document.querySelector('.modal__title'),
+  genresList = document.querySelector('.genres-list'),
+  rating = document.querySelector('.rating'),
+  description = document.querySelector('.description'),
+  modalLink = document.querySelector('.modal__link'),
+  searchForm = document.querySelector('.search__form'),
+  searchFormInput = document.querySelector('.search__form-input');
+
+
+const loading = document.createElement('div');
+loading.className = 'loading';
 
 
 const DBService = class {
+  constructor() {
+    this.SERVER = 'https://api.themoviedb.org/3';
+    this.API_KEY = 22%5 + 'd4c165832ba9416909b2d8884eb29e' + 23%5;
+  }
+
   getData = async url => {
     const res = await fetch(url);
     if(res.ok) {
@@ -22,11 +39,21 @@ const DBService = class {
   getTestData = () => {
     return this.getData('test.json');
   }
+
+  getTestCard = () => {
+    return this.getData('card.json');
+  }
+
+  getSearchResult = query => this
+    .getData(`${this.SERVER}/search/tv?api_key=${this.API_KEY}&language=ru-RU&page=1&query=${query}&include_adult=true`);
+
+  getTvShow = id => this
+    .getData(`${this.SERVER}/tv/${id}?api_key=${this.API_KEY}&language=ru-RU`);
 }
 
+// console.log(new DBService().getSearchResult('Папа'));
 
 const renderCard = response => {
-  console.log(response);
   tvShowsList.textContent = '';
 
   response.results.forEach(item => {
@@ -34,7 +61,8 @@ const renderCard = response => {
             backdrop_path: backdrop,
             name: title,
             poster_path: poster,
-            vote_average: vote
+            vote_average: vote,
+            id
           } = item;
 
     const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
@@ -44,7 +72,7 @@ const renderCard = response => {
     const card = document.createElement('li');
     card.className = 'tv-shows__item';
     card.innerHTML = `
-      <a href="#" class="tv-card">
+      <a href="#" id="${id}" class="tv-card">
           ${voteElem}
           <img class="tv-card__img"
                src="${posterIMG}"
@@ -53,13 +81,21 @@ const renderCard = response => {
           <h4 class="tv-card__head">${title}</h4>
       </a>
     `;
-
+    // console.dir(card);
+    loading.remove();
     tvShowsList.append(card);
-    console.log(card);
   });
 };
 
-new DBService().getTestData().then(renderCard);
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const value = searchFormInput.value.trim();
+  searchFormInput.value = '';
+  if(value) {
+    tvShows.append(loading);
+    new DBService().getSearchResult(value).then(renderCard);
+  }
+});
 
 
 
@@ -77,6 +113,7 @@ document.addEventListener('click', e => {
 });
 
 leftMenu.addEventListener('click', e => {
+  e.preventDefault();
   const target = e.target;
   const dropdown = target.closest('.dropdown');
 
@@ -112,8 +149,27 @@ tvShowsList.addEventListener('click', e => {
   const card = target.closest('.tv-card');
 
   if(card) {
-    document.body.style.overflow = 'hidden';
-    modal.classList.remove('hide');
+    new DBService().getTvShow(card.id)
+      .then(({
+              poster_path: posterPath,
+              name: title,
+              genres,
+              vote_average: voteAverage,
+              overview,
+              homepage
+            }) => {        
+        tvCardImg.src = IMG_URL + posterPath;
+        tvCardImg.alt = title;
+        modalTitle.textContent = title;
+        genresList.innerHTML = genres.reduce((acc, item) =>  `${acc}<li>${item.name}</li>`, '');
+        rating.textContent = voteAverage;
+        description.textContent = overview;
+        modalLink.href = homepage;
+      })
+      .then(() => {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+      });
   }
 });
 
