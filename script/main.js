d31a608adcab11d354e02ddb8985427430b565d2
Denis <niskadevla@gmail.com>
2020-05-28 10:21:23 +0300

    Day 2, and Home work

--- script/main.js
+++ script/main.js
@@ -1,8 +1,67 @@
 'use strict';
 
-const leftMenu = document.querySelector('.left-menu');
-const hamburger = document.querySelector('.hamburger');
-const tvShows = document.querySelector('.tv-shows');
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = '2d4c165832ba9416909b2d8884eb29e3';
+
+const leftMenu = document.querySelector('.left-menu'),
+  hamburger = document.querySelector('.hamburger'),
+  tvShowsList = document.querySelector('.tv-shows__list'),
+  modal = document.querySelector('.modal');
+
+
+const DBService = class {
+  getData = async url => {
+    const res = await fetch(url);
+    if(res.ok) {
+      return res.json();
+    } else {
+      throw new Error(`Did not get data ${url}`);
+    }
+  }
+
+  getTestData = () => {
+    return this.getData('test.json');
+  }
+}
+
+
+const renderCard = response => {
+  console.log(response);
+  tvShowsList.textContent = '';
+
+  response.results.forEach(item => {
+    const {
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote
+          } = item;
+
+    const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+    const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+    const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+
+    const card = document.createElement('li');
+    card.className = 'tv-shows__item';
+    card.innerHTML = `
+      <a href="#" class="tv-card">
+          ${voteElem}
+          <img class="tv-card__img"
+               src="${posterIMG}"
+               data-backdrop="${backdropIMG}"
+               alt="${title}">
+          <h4 class="tv-card__head">${title}</h4>
+      </a>
+    `;
+
+    tvShowsList.append(card);
+    console.log(card);
+  });
+};
+
+new DBService().getTestData().then(renderCard);
+
+
 
 //Opening and closing the menu
 hamburger.addEventListener('click', () => {
@@ -28,15 +87,42 @@ leftMenu.addEventListener('click', e => {
   }
 });
 
-tvShows.addEventListener('mouseover', e => {
-  const target = e.target;
-  const tvCard = target.closest('.tv-card');
+//Changing card
+const changeImage = e => {
+  const card = e.target.closest('.tv-shows__item');
 
-  if(tvCard) {
-    const img = tvCard.querySelector('.tv-card__img');
-    const src = img.src;
+  if(card) {
+    const img  = card.querySelector('.tv-card__img');
 
-    img.src = img.dataset.backdrop;
-    tvCard.onmouseout = () => img.src = src;
+    if(img.dataset.backdrop) {
+      [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
+    }
   }
-})
+}
+
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);
+
+
+//Opening modal window
+tvShowsList.addEventListener('click', e => {
+  e.preventDefault();
+
+  const target = e.target;
+  const card = target.closest('.tv-card');
+
+  if(card) {
+    document.body.style.overflow = 'hidden';
+    modal.classList.remove('hide');
+  }
+});
+
+//Closing modal window
+modal.addEventListener('click', e => {
+  // console.log(e.target.classList.contains('modal'));
+  if(e.target.closest('.cross') ||
+    e.target.classList.contains('modal')) {
+    document.body.style.overflow = '';
+    modal.classList.add('hide');
+  }
+});
